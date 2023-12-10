ee3b46e20de673f01d9cc036fb789b93c8e0d93b
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2023-12-10 12:18:14 +0530

    client side login and register form was impliment

--- client/doctor-appointment/src/App.js
+++ client/doctor-appointment/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div >
-        hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/doctor-appointment/src/pages/Login.js
+++ client/doctor-appointment/src/pages/Login.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Form, Input } from 'antd';
+import '../style/RegisterStyles.css'
+import { Link } from 'react-router-dom'
 const Login = () => {
-return (
-    <div>Login</div>
-)
+    const onFinishHandler = (values) => {
+        console.log(values)
+    }
+    return (
+        <div className='form-container'>
+            <Form name="basic" layout='vertical' onFinish={onFinishHandler} className='register-form card'>
+                <h1>Login Form</h1>
+                <Form.Item label="email" name="email" rules={[{ required: true, message: 'Please input your name!', },]}>
+                    <Input type='email' required />
+                </Form.Item>
+                <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!', },]}>
+                    <Input type="password" required />
+                </Form.Item>
+                <Link to='/login'>Already user Login here</Link>
+                <button className='btn btn-primary' type='submit'>
+                    Submit
+                </button>
+            </Form>
+        </div>
+    )
 }
 
 export default Login
