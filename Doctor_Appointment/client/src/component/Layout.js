1ca95f4efb134b1eef56fd85072ae8cfdec117dd
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-01-04 19:50:40 +0530

    navbar and logo

--- Doctor_Appointment/client/src/component/Layout.js
+++ Doctor_Appointment/client/src/component/Layout.js
@@ -1,24 +1,50 @@
-import React from 'react'
-import {doctor } from '../images/Images'
-import { Avatar } from 'antd'
-
-const Layout = ({children}) => {
+import React, { useState } from 'react'
+import { doctor } from '../images/Images'
+import { Avatar, Button } from 'antd'
+import { SidebarMenu } from './data'
+import { Link, useLocation } from 'react-router-dom'
+const Layout = ({ children }) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const location = useLocation()
     return (
-        <div className='main'>
+        <div className={`main ${collapsed ? 'collapsed' : ''}`}>
             <div className='layout'>
                 <div className='sidebar'>
-                    <div className='logo'>
-                    <Avatar size={64} src={doctor} />
+                    <Button
+                        type="text"
+                        icon={collapsed ? <i class="fa-solid fa-bars"></i> : <i class="fa-solid fa-bars"></i>}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            color: '#fff',
+                            fontSize: '26px',
+                            width: 30,
+                            height: 30,
+                        }}
+                    />
+
+                    <div className='menu'>
+                        {SidebarMenu.map(menu => {
+                            const isActive = location.pathname === menu.path
+                            return (
+                                <>
+                                    <div className={`menu-item ${isActive && "active"}`}>
+                                        <i className={menu.icon}></i>
+                                        <Link Link={menu.path}>{menu.name}</Link>
+                                    </div>
+                                </>
+                            )
+                        })}
                     </div>
-                    <div className='menu'>Menu</div>
                 </div>
                 <div className='content'>
                     <div className='header'>
-                    <div className='header'>Header</div>
+                    <div className='logo'>
+                                <Avatar size={55} src={doctor} />
+                            </div>
                     </div>
                     <div className='body'>
                         <div className='body'>{children}</div>
-                        
+
                     </div>
                 </div>
             </div>
--- Doctor_Appointment/client/src/component/data.js
+++ Doctor_Appointment/client/src/component/data.js
@@ -1,6 +1,27 @@
 export const SidebarMenu = [
     {
         name:'Home',
-        path:'/'
+        path:'/',
+        icon:'fa-solid fa-house'
+    },
+    {
+        name:'Appointments',
+        path:'/appointment',
+        icon:'fa-solid fa-calendar-check'
+    },
+    {
+        name:'Apply Doctor',
+        path:'/apply-doctor',
+        icon:'fa-solid fa-user-doctor'
+    },
+    {
+        name:'Profile',
+        path:'/profile',
+        icon:'fa-solid fa-user'
+    },
+    {
+        name:'Logout',
+        path:'/logout',
+        icon:'fa-solid fa-right-from-bracket'
     }
 ]
